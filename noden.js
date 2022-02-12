fac2093e0376a770459be2f55d4077de96de75f7
Saqlanekhan <Saqlainsanha9845@gmail.com>
2022-02-12 14:21:58 +0530

    express2

--- noden.js
+++ noden.js
@@ -17,11 +17,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRouter);
 app.use(shopRouter);
 app.use(contactRouter)
-// app.get('/contactus',(req,res,next) =>{
-//        console.log('contact')
-//            res.sendFile(path.join(rootDir,'views','contact.html'));
-        
-//         })
+
 
 
 app.use((req,res,next) =>{
